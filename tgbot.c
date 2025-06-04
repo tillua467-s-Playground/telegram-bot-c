b137a6ebf4724bbd157af2f7c55a42f2ee7b77f3
tillua467 <128046674+tillua467@users.noreply.github.com>
2025-06-04 20:50:41 +0600

    tgbot: Add send_message() function
    *logic*
    -Accept 3 type of data whom to message (their message id), what meesage
     and what message to reply(if don't want to reply set it to -1) then
     create a link that we will use to make a http request and send the
     message
    - Also we don't know how big the message is going to be so let's use
      malloc to make sure that the code has enough space to create that big
      url
    
    Signed-off-by: tillua467 <128046674+tillua467@users.noreply.github.com>

--- tgbot.c
+++ tgbot.c
@@ -8,14 +8,18 @@
 #include <sys/stat.h>
 #include <time.h>
 
-struct Mem_struct {
+#define BOT_API "NULL"
+#define Tg_link "https://api.telegram.org/bot" BOT_API "/"
+
+
+typedef struct Mem_struct {
     char *memory;
     size_t size;
-};
+}Mem_struct;
 
 size_t callback(char *ptr, size_t size, size_t nmemb, void *userdata){
     size_t total_size = size * nmemb;
-    struct Mem_struct *mem = (struct Mem_struct *)userdata;
+    Mem_struct *mem = (struct Mem_struct *)userdata;
 
     char *tempptr = realloc(mem->memory, mem->size + total_size + 1);
 
@@ -32,4 +36,47 @@ size_t callback(char *ptr, size_t size, size_t nmemb, void *userdata){
     return total_size;
 }
 
-int main(){ return 0;}
+void send_message(long long *CID, const char *message, long long reply_id){
+    CURL *curl;
+    CURLcode res;
+    Mem_struct chunk;
+    chunk.memory = malloc(1);
+    chunk.size = 0;
+    char *url;
+
+    curl = curl_easy_init();
+
+    if(curl){
+        char *escaped_message = curl_easy_escape(curl, message, 0);
+
+        if(reply_id == -1){
+            int url_length = snprintf(NULL, 0, "%ssendMessage?chat_id=%lld&text=%s", Tg_link, *CID, escaped_message);
+            url = malloc(url_length + 1);
+            snprintf(url, url_length + 1, "%ssendMessage?chat_id=%lld&text=%s", Tg_link, *CID, escaped_message);
+            curl_free(escaped_message);
+        } else{
+            int url_length = snprintf(NULL, 0, "%ssendMessage?chat_id=%lld&text=%s&reply_to_message=%lld", Tg_link, *CID, escaped_message, reply_id);
+            url = malloc(url_length + 1);
+            snprintf(url, url_length, "%ssendMessage?chat_id=%lld&text=%s&reply_to_message=%lld", Tg_link, *CID, escaped_message, reply_id);
+            curl_free(escaped_message);
+        }
+
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+
+        res = curl_easy_perform(curl);
+
+        if(res != CURLE_OK){
+            fprintf(stderr, "Failed: %s\n", curl_easy_strerror(res));
+        }
+        printf("Measage was sent to %lld successfully", CID);
+        printf("server response:\n%s\n", chunk.memory);
+
+        curl_easy_cleanup(curl);
+        free(chunk.memory);
+        free(url);
+    } else{
+        fprintf(stderr, "Failed to initialize cURL\n");
+    }
+}
